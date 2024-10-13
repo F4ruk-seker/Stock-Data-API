a14a4715f69448cbdc17c9528902d16198d5250c
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-10-13 19:37:22 +0300

    Add header

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,42 @@
 <template>
+  <AppHeader />
+
+  <div class="overflow-x-auto">
+  <table class="table table-zebra">
+    <!-- head -->
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- row 1 -->
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      <!-- row 2 -->
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      <!-- row 3 -->
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -6,4 +44,5 @@
 </template>
 
 <script setup>
+import AppHeader from "@/components/AppHeader.vue";
 </script>
